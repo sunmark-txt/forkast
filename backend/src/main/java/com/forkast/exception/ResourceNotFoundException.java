a7a6319a86cc78a595a74db16a9e87648574c462
sunmark-txt <andingm358@gmail.com>
2025-05-21 15:56:02 +0800

    Delete backend directory

--- backend/src/main/java/com/forkast/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.forkast.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-    }
-} 
